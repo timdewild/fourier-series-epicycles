bf0f6ed8849c43addbc26ebff4c4ed4d8c3c92e0
Tim de Wild <timdewild97@hotmail.nl>
2024-03-22 20:31:33 +0100

    Add AnimatedTrace to matnimation submodule

--- fourier_series_epicycles.py
+++ fourier_series_epicycles.py
@@ -8,9 +8,10 @@ sys.path.append(os.path.abspath(abs_path))
 
 from matnimation.src.matnimation.animation.animation import Animation
 from matnimation.src.matnimation.canvas.single_canvas import SingleCanvas
+from matnimation.src.matnimation.canvas.multi_canvas import MultiCanvas
 from matnimation.src.matnimation.artist.animated.animated_line import AnimatedLine
-from matnimation.src.matnimation.artist.static.static_line import StaticLine
 from matnimation.src.matnimation.artist.animated.animated_circle import AnimatedCircle
+from matnimation.src.matnimation.artist.animated.animated_arrow import AnimatedArrow
 
 from fourier_vectors_evolution import FourierVectorsEvolution
 
@@ -23,11 +24,11 @@ def complex_coefficients_square_wave(n: int):
 
     return cn 
 
-order = 20
+order = 10
 coefficients = [complex_coefficients_square_wave(k) for k in np.linspace(-order, order, 2 * order + 1)]
 coefficients = np.asarray(coefficients)
 
-time = np.linspace(0,1,1000)
+time = np.linspace(0,1,700)
 
 test = FourierVectorsEvolution(
     fourier_coefficients = coefficients, 
@@ -49,27 +50,63 @@ cycles_radii = test.get_cycles_radii()
 
 
 #---- ANIMATION ---#
-canvas = SingleCanvas(
-    figsize = (4,4),
+
+canvas = MultiCanvas(
+    figsize = (8,4),
     dpi = 400,
     time_array = time,
-    axis_limits = [-2,2,-2,2],
-    axis_labels = ['$x$','$y$']
+    nrows = 1,
+    ncols = 2,
+    axes_limits = [ [0,1,-1.5,+1.5], [-1.5, 1.5, -1.5, 1.5] ],
+    axes_labels = [[' ', ' '], [' ', ' ']]
 )
 
-canvas.save_canvas('testcanvas.jpg')
+canvas.set_axis_properties(row = 0, col = 0, xticks = [0, 0.25, 0.5, 0.75, 1], xticklabels = ['$0$', '', '$0.5$', '', '$1$'], yticklabels = ['', '$-1$', '' , '$0$', '', '$+1$', ''])
+canvas.set_axis_properties(row = 0, col = 1, xticklabels = [], yticklabels = [])
 
-vectors = AnimatedLine(
-    name = 'vectors',
-    x_data = ydata,
-    y_data = xdata
-)
+canvas.save_canvas('canvas.jpg')
 
-vectors.set_styling_properties(linewidth = 0.5)
+# canvas = SingleCanvas(
+#     figsize = (4,4),
+#     dpi = 400,
+#     time_array = time,
+#     axis_limits = [-1.5,1.5,-1.5,1.5],
+#     axis_labels = [' ', ' ']
+# )
+# canvas.set_axis_properties(xticklabels = [], yticklabels = [])
+# canvas.save_canvas('testcanvas.jpg')
 
-canvas.add_artist(vectors)
 
-for i in range(1,2 * order + 1):
+vector_width = 0.0001
+head_scale = 1.2732 * 0.07
+
+for i in range(1, 2 * order + 1):
+    x_tail_data = ydata[i - 1, :]
+    y_tail_data = xdata[i - 1, :]
+
+    x_tip_data = ydata[i, :]
+    y_tip_data = xdata[i, :]
+
+    head_width = head_scale * cycles_radii[i]
+
+    vector = AnimatedArrow(
+        name = f"vector-{i}",
+        x_tail_data = x_tail_data,
+        y_tail_data = y_tail_data,
+        x_tip_data = x_tip_data,
+        y_tip_data = y_tip_data,
+        width = vector_width,
+        head_width = head_width,       #3 * head_scale * vector_width,
+        head_length = 1.5 * head_width      #1.5 * 3 * head_scale * vector_width
+    )
+
+    canvas.add_artist(vector, row = 0, col = 1)
+
+    vector.set_styling_properties(linewidth = 0.4, facecolor = 'k', edgecolor = 'k')
+
+print(cycles_radii[1])
+
+for i in range(1, 2 * order + 1):
     x_center = ydata[i - 1, :]
     y_center = xdata[i - 1, :]
     cycle_radius = cycles_radii[i]
@@ -81,7 +118,7 @@ for i in range(1,2 * order + 1):
         y_data = y_center
     )
 
-    canvas.add_artist(cycle)
+    canvas.add_artist(cycle, row = 0, col = 1)
 
     cycle.set_styling_properties(linewidth = 0.2, edgecolor = 'k', facecolor = 'None', alpha = 0.5)
 
@@ -95,7 +132,7 @@ tracing_dot = AnimatedCircle(
     y_data = y_dot
 )
 
-canvas.add_artist(tracing_dot)
+canvas.add_artist(tracing_dot, row = 0, col = 1)
 
 tracing_dot.set_styling_properties(edgecolor = 'tab:red', facecolor = 'tab:red')
 
